491358c2e1281d52c88f5c3dbab02210483c77cc
SuperToaster2001 <36634189+SuperToaster2001@users.noreply.github.com>
2020-11-01 22:37:30 +0000

    Re-Wrote the hole thing in C#
    Re-Wrote the hole thing in C# because I was having trouble with java

---
+++ MainForm.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace esi
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.check_discord = new System.Windows.Forms.CheckBox();
+            this.install_button = new System.Windows.Forms.Button();
+            this.check_hwmonitor = new System.Windows.Forms.CheckBox();
+            this.check_java = new System.Windows.Forms.CheckBox();
+            this.check_zoom = new System.Windows.Forms.CheckBox();
+            this.check_filezilla = new System.Windows.Forms.CheckBox();
+            this.check_flux = new System.Windows.Forms.CheckBox();
+            this.check_github = new System.Windows.Forms.CheckBox();
+            this.check_powertoys = new System.Windows.Forms.CheckBox();
+            this.check_qbittorrent = new System.Windows.Forms.CheckBox();
+            this.check_visualstudio = new System.Windows.Forms.CheckBox();
+            this.check_operagx = new System.Windows.Forms.CheckBox();
+            this.check_chrome = new System.Windows.Forms.CheckBox();
+            this.check_firefox = new System.Windows.Forms.CheckBox();
+            this.check_opera = new System.Windows.Forms.CheckBox();
+            this.check_raidrive = new System.Windows.Forms.CheckBox();
+            this.check_rainmeter = new System.Windows.Forms.CheckBox();
+            this.support_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // check_discord
+            // 
+            this.check_discord.AutoSize = true;
+            this.check_discord.Location = new System.Drawing.Point(12, 12);
+            this.check_discord.Name = "check_discord";
+            this.check_discord.Size = new System.Drawing.Size(62, 17);
+            this.check_discord.TabIndex = 0;
+            this.check_discord.Text = "Discord";
+            this.check_discord.UseVisualStyleBackColor = true;
+            // 
+            // install_button
+            // 
+            this.install_button.Location = new System.Drawing.Point(152, 261);
+            this.install_button.Name = "install_button";
+            this.install_button.Size = new System.Drawing.Size(75, 23);
+            this.install_button.TabIndex = 1;
+            this.install_button.Text = "Install";
+            this.install_button.UseVisualStyleBackColor = true;
+            this.install_button.Click += new System.EventHandler(this.install_button_Click);
+            // 
+            // check_hwmonitor
+            // 
+            this.check_hwmonitor.AutoSize = true;
+            this.check_hwmonitor.Location = new System.Drawing.Point(12, 35);
+            this.check_hwmonitor.Name = "check_hwmonitor";
+            this.check_hwmonitor.Size = new System.Drawing.Size(110, 17);
+            this.check_hwmonitor.TabIndex = 2;
+            this.check_hwmonitor.Text = "Hardware Monitor";
+            this.check_hwmonitor.UseVisualStyleBackColor = true;
+            // 
+            // check_java
+            // 
+            this.check_java.AutoSize = true;
+            this.check_java.Location = new System.Drawing.Point(12, 58);
+            this.check_java.Name = "check_java";
+            this.check_java.Size = new System.Drawing.Size(49, 17);
+            this.check_java.TabIndex = 3;
+            this.check_java.Text = "Java";
+            this.check_java.UseVisualStyleBackColor = true;
+            // 
+            // check_zoom
+            // 
+            this.check_zoom.AutoSize = true;
+            this.check_zoom.Location = new System.Drawing.Point(12, 81);
+            this.check_zoom.Name = "check_zoom";
+            this.check_zoom.Size = new System.Drawing.Size(53, 17);
+            this.check_zoom.TabIndex = 4;
+            this.check_zoom.Text = "Zoom";
+            this.check_zoom.UseVisualStyleBackColor = true;
+            // 
+            // check_filezilla
+            // 
+            this.check_filezilla.AutoSize = true;
+            this.check_filezilla.Location = new System.Drawing.Point(12, 104);
+            this.check_filezilla.Name = "check_filezilla";
+            this.check_filezilla.Size = new System.Drawing.Size(59, 17);
+            this.check_filezilla.TabIndex = 5;
+            this.check_filezilla.Text = "Filezilla";
+            this.check_filezilla.UseVisualStyleBackColor = true;
+            // 
+            // check_flux
+            // 
+            this.check_flux.AutoSize = true;
+            this.check_flux.Location = new System.Drawing.Point(12, 127);
+            this.check_flux.Name = "check_flux";
+            this.check_flux.Size = new System.Drawing.Size(45, 17);
+            this.check_flux.TabIndex = 6;
+            this.check_flux.Text = "f.lux";
+            this.check_flux.UseVisualStyleBackColor = true;
+            // 
+            // check_github
+            // 
+            this.check_github.AutoSize = true;
+            this.check_github.Location = new System.Drawing.Point(12, 150);
+            this.check_github.Name = "check_github";
+            this.check_github.Size = new System.Drawing.Size(102, 17);
+            this.check_github.TabIndex = 7;
+            this.check_github.Text = "GitHub Desktop";
+            this.check_github.UseVisualStyleBackColor = true;
+            // 
+            // check_powertoys
+            // 
+            this.check_powertoys.AutoSize = true;
+            this.check_powertoys.Location = new System.Drawing.Point(12, 173);
+            this.check_powertoys.Name = "check_powertoys";
+            this.check_powertoys.Size = new System.Drawing.Size(79, 17);
+            this.check_powertoys.TabIndex = 8;
+            this.check_powertoys.Text = "PowerToys";
+            this.check_powertoys.UseVisualStyleBackColor = true;
+            // 
+            // check_qbittorrent
+            // 
+            this.check_qbittorrent.AutoSize = true;
+            this.check_qbittorrent.Location = new System.Drawing.Point(12, 196);
+            this.check_qbittorrent.Name = "check_qbittorrent";
+            this.check_qbittorrent.Size = new System.Drawing.Size(71, 17);
+            this.check_qbittorrent.TabIndex = 9;
+            this.check_qbittorrent.Text = "qBittorent";
+            this.check_qbittorrent.UseVisualStyleBackColor = true;
+            // 
+            // check_visualstudio
+            // 
+            this.check_visualstudio.AutoSize = true;
+            this.check_visualstudio.Location = new System.Drawing.Point(12, 219);
+            this.check_visualstudio.Name = "check_visualstudio";
+            this.check_visualstudio.Size = new System.Drawing.Size(87, 17);
+            this.check_visualstudio.TabIndex = 10;
+            this.check_visualstudio.Text = "Visual Studio";
+            this.check_visualstudio.UseVisualStyleBackColor = true;
+            // 
+            // check_operagx
+            // 
+            this.check_operagx.AutoSize = true;
+            this.check_operagx.Location = new System.Drawing.Point(128, 35);
+            this.check_operagx.Name = "check_operagx";
+            this.check_operagx.Size = new System.Drawing.Size(70, 17);
+            this.check_operagx.TabIndex = 11;
+            this.check_operagx.Text = "OperaGX";
+            this.check_operagx.UseVisualStyleBackColor = true;
+            // 
+            // check_chrome
+            // 
+            this.check_chrome.AutoSize = true;
+            this.check_chrome.Location = new System.Drawing.Point(128, 58);
+            this.check_chrome.Name = "check_chrome";
+            this.check_chrome.Size = new System.Drawing.Size(99, 17);
+            this.check_chrome.TabIndex = 12;
+            this.check_chrome.Text = "Google Chrome";
+            this.check_chrome.UseVisualStyleBackColor = true;
+            // 
+            // check_firefox
+            // 
+            this.check_firefox.AutoSize = true;
+            this.check_firefox.Location = new System.Drawing.Point(128, 81);
+            this.check_firefox.Name = "check_firefox";
+            this.check_firefox.Size = new System.Drawing.Size(92, 17);
+            this.check_firefox.TabIndex = 13;
+            this.check_firefox.Text = "Mozilla Firefox";
+            this.check_firefox.UseVisualStyleBackColor = true;
+            // 
+            // check_opera
+            // 
+            this.check_opera.AutoSize = true;
+            this.check_opera.Location = new System.Drawing.Point(128, 12);
+            this.check_opera.Name = "check_opera";
+            this.check_opera.Size = new System.Drawing.Size(55, 17);
+            this.check_opera.TabIndex = 14;
+            this.check_opera.Text = "Opera";
+            this.check_opera.UseVisualStyleBackColor = true;
+            // 
+            // check_raidrive
+            // 
+            this.check_raidrive.AutoSize = true;
+            this.check_raidrive.Location = new System.Drawing.Point(128, 104);
+            this.check_raidrive.Name = "check_raidrive";
+            this.check_raidrive.Size = new System.Drawing.Size(67, 17);
+            this.check_raidrive.TabIndex = 15;
+            this.check_raidrive.Text = "RaiDrive";
+            this.check_raidrive.UseVisualStyleBackColor = true;
+            // 
+            // check_rainmeter
+            // 
+            this.check_rainmeter.AutoSize = true;
+            this.check_rainmeter.Location = new System.Drawing.Point(128, 127);
+            this.check_rainmeter.Name = "check_rainmeter";
+            this.check_rainmeter.Size = new System.Drawing.Size(74, 17);
+            this.check_rainmeter.TabIndex = 16;
+            this.check_rainmeter.Text = "Rainmeter";
+            this.check_rainmeter.UseVisualStyleBackColor = true;
+            // 
+            // support_button
+            // 
+            this.support_button.Location = new System.Drawing.Point(12, 261);
+            this.support_button.Name = "support_button";
+            this.support_button.Size = new System.Drawing.Size(75, 23);
+            this.support_button.TabIndex = 24;
+            this.support_button.Text = "Support Me";
+            this.support_button.UseVisualStyleBackColor = true;
+            this.support_button.Click += new System.EventHandler(this.support_button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(128, 151);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 39);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "More Installers\r\nWill be\r\n Available Soon!";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(238, 297);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.support_button);
+            this.Controls.Add(this.check_rainmeter);
+            this.Controls.Add(this.check_raidrive);
+            this.Controls.Add(this.check_opera);
+            this.Controls.Add(this.check_firefox);
+            this.Controls.Add(this.check_chrome);
+            this.Controls.Add(this.check_operagx);
+            this.Controls.Add(this.check_visualstudio);
+            this.Controls.Add(this.check_qbittorrent);
+            this.Controls.Add(this.check_powertoys);
+            this.Controls.Add(this.check_github);
+            this.Controls.Add(this.check_flux);
+            this.Controls.Add(this.check_filezilla);
+            this.Controls.Add(this.check_zoom);
+            this.Controls.Add(this.check_java);
+            this.Controls.Add(this.check_hwmonitor);
+            this.Controls.Add(this.install_button);
+            this.Controls.Add(this.check_discord);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Essential Software Installer by Tiago Matos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox check_discord;
+        private System.Windows.Forms.Button install_button;
+        private System.Windows.Forms.CheckBox check_hwmonitor;
+        private System.Windows.Forms.CheckBox check_java;
+        private System.Windows.Forms.CheckBox check_zoom;
+        private System.Windows.Forms.CheckBox check_filezilla;
+        private System.Windows.Forms.CheckBox check_flux;
+        private System.Windows.Forms.CheckBox check_github;
+        private System.Windows.Forms.CheckBox check_powertoys;
+        private System.Windows.Forms.CheckBox check_qbittorrent;
+        private System.Windows.Forms.CheckBox check_visualstudio;
+        private System.Windows.Forms.CheckBox check_operagx;
+        private System.Windows.Forms.CheckBox check_chrome;
+        private System.Windows.Forms.CheckBox check_firefox;
+        private System.Windows.Forms.CheckBox check_opera;
+        private System.Windows.Forms.CheckBox check_raidrive;
+        private System.Windows.Forms.CheckBox check_rainmeter;
+        private System.Windows.Forms.Button support_button;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace esi
+{
+    public partial class MainForm : Form
+    {
+        #region messages
+        readonly string message_standard = "Press Ok to Install";
+        readonly string message_support = "Please consider helping me financially";
+        #endregion
+
+        public string folder = AppDomain.CurrentDomain.BaseDirectory + "installers\\";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void install_button_Click(object sender, EventArgs e)
+        {
+            
+            if (check_discord.Checked == true)
+            {
+                MessageBox.Show(message_standard +" Discord");
+                try
+                {
+                    Process.Start(folder + "DiscordSetup.exe");
+                }
+                catch { }
+            }
+            if (check_hwmonitor.Checked == true)
+            {
+                MessageBox.Show(message_standard + " Hardware Monitor");
+                try
+                {
+                    Process.Start(folder + "hwmonitor.exe");
+                }
+                catch { }
+                
+            }
+            if (check_java.Checked == true)
+            {
+                MessageBox.Show(message_standard + " Java");
+                try
+                {
+                    Process.Start(folder + "java.exe");
+                }
+                catch{ }
+            }
+            if (check_zoom.Checked == true)
+            {
+                MessageBox.Show(message_standard + " Zoom");
+                try
+                {
+                    Process.Start(folder + "ZoomInstaller.exe");
+                }
+                catch { }                
+            }
+            if (check_filezilla.Checked == true)
+            {
+                MessageBox.Show(message_standard + " Filezilla");
+                try
+                {
+                    Process.Start(folder + "Filezilla-setup.exe");
+                }
+                catch { }
+            }
+            if (check_flux.Checked == true)
+            {
+                MessageBox.Show(message_standard + " f.lux");
+                try
+                {
+                    Process.Start(folder + "f-lux-setup.exe");
+                }
+                catch { }
+            }
+            if (check_github.Checked == true)
+            {
+                MessageBox.Show(message_standard + " GitHub Desktop");
+                try
+                {
+                    Process.Start(folder + "GitHubDesktopSetup.exe");
+                }
+                catch { }
+            }
+            if (check_powertoys.Checked == true)
+            {
+                MessageBox.Show(message_standard + " PowerToys");
+                try
+                {
+                    Process.Start(folder + "PowerToysSetup-0.23.2-x64.exe");
+                }
+                catch { }
+            }
+            if (check_qbittorrent.Checked == true)
+            {
+                MessageBox.Show(message_standard + " qBittorent");
+                try
+                {
+                    Process.Start(folder + "qbittorrent_4.3.0.1_x64_setup.exe");
+                }
+                catch { }
+            }
+            if (check_visualstudio.Checked == true)
+            {
+                MessageBox.Show(message_standard + " Visual Studio");
+                try
+                {
+                    Process.Start(folder + "vs_community__452317010.1603328756.exe");
+                }
+                catch { }
+            }
+            if (check_opera.Checked == true)
+            {
+                MessageBox.Show(message_standard + " Opera");
+                try
+                {
+                    Process.Start(folder + "OperaSetup.exe");
+                }
+                catch { }
+            }
+            if (check_operagx.Checked == true)
+            {
+                MessageBox.Show(message_standard + " OperaGX");
+                try
+                {
+                    Process.Start(folder + "OperaGXSetup.exe");
+                }
+                catch { }
+            }
+            if (check_chrome.Checked == true)
+            {
+                MessageBox.Show(message_standard + " Google Chrome");
+                try
+                {
+                    Process.Start(folder + "ChromeSetup.exe");
+                }
+                catch { }
+            }
+            if (check_firefox.Checked == true)
+            {
+                MessageBox.Show(message_standard + " Mozilla Firefox");
+                try
+                {
+                    Process.Start(folder + "Firefox Installer.exe");
+                }
+                catch { }
+            }
+            if (check_raidrive.Checked == true)
+            {
+                MessageBox.Show(message_standard + " RaiDrive");
+                try
+                {
+                    Process.Start(folder + "raidrive-2020-6-80.exe");
+                }
+                catch { }
+            }
+            if (check_rainmeter.Checked == true)
+            {
+                MessageBox.Show(message_standard + " Rainmeters");
+                try
+                {
+                    Process.Start(folder + "Rainmeter-4.3.1.exe");
+                }
+                catch { }
+            }
+            if (check_raidrive.Checked == true)
+            {
+                MessageBox.Show(message_standard + " RaiDrive");
+                try
+                {
+                    Process.Start(folder + "raidrive-2020-6-80.exe");
+                }
+                catch { }
+            }
+        }
+        private void support_button_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show(message_support, "Confirm", MessageBoxButtons.OKCancel, MessageBoxIcon.Information) == DialogResult.OK)
+            {
+                Process.Start("https://www.paypal.me/toastaru");
+            }
+        }
+    }
+}
